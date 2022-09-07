06bc91c8d7959bfd425e0a509db9c77b72468921
yuki-shimojo269 <shyuki65@gmail.com>
2022-09-07 23:39:51 +0900

    remove test

--- test_code/hello_world.go
+++
@@ -1,7 +0,0 @@
-package main		// mainパッケージであることを宣言
-
-import "fmt"		// fmtモジュールをインポート
-
-func main() {		// 最初に実行されるmain()関数を定義
-    fmt.Println("hello, world")
-}
--- test_code/serch_menu/go.mod
+++
@@ -1,9 +0,0 @@
-module serch_menue
-
-go 1.15
-
-require (
-	github.com/PuerkitoBio/goquery v1.8.0
-	gorm.io/driver/sqlite v1.3.2
-	gorm.io/gorm v1.23.5
-)
